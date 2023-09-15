a731ebf8c77bf4b928f27d46f2e87317afd296ab
Ariel Castro <arielcastron22@hotmail.com>
2023-09-15 08:39:22 -0300

    um monte de att ai

--- src/app/shared/banner/banner.component.ts
+++ src/app/shared/banner/banner.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-banner',
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BannerComponent implements OnInit {
 
+  @Input() src: string = ''
+  @Input() alt: string = ''
   constructor() { }
 
   ngOnInit(): void {
   }
+
+
 
 }
