e5a5a46f1b3d178377d7393e4bba08420abe2e7d
Ariel Castro <arielcastron22@hotmail.com>
2023-09-19 07:49:48 -0300

    correção do card app card removendo o selector

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,9 @@ import { HomeComponent } from './pages/home/home.component';
 import { CardBuscaComponent } from './shared/card-busca/card-busca.component';
 import { MatCardModule } from '@angular/material/card';
 import { CardDepoimentosComponent } from './shared/card-depoimentos/card-depoimentos.component';
+import { FormBuscaComponent } from './shared/form-busca/form-busca.component';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -27,6 +30,7 @@ import { CardDepoimentosComponent } from './shared/card-depoimentos/card-depoime
     HomeComponent,
     CardBuscaComponent,
     CardDepoimentosComponent,
+    FormBuscaComponent,
   ],
   imports: [
     BrowserModule,
@@ -35,6 +39,8 @@ import { CardDepoimentosComponent } from './shared/card-depoimentos/card-depoime
     MatToolbarModule,
     MatButtonModule,
     MatCardModule,
+    MatButtonToggleModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/shared/form-busca/form-busca.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-busca',
+  templateUrl: './form-busca.component.html',
+  styleUrls: ['./form-busca.component.scss']
+})
+export class FormBuscaComponent{
+
+  constructor() { }
+
+}
